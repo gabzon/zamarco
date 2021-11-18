755302f2779c1b15b83e1450703e33351153f79b
Gabriel Zambrano <gab.zambrano@gmail.com>
2021-11-18 17:38:30 +0100

    export improvements

--- app/Exports/TransactionExport.php
+++ app/Exports/TransactionExport.php
@@ -7,8 +7,10 @@ use App\Models\Transaction;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
-class TransactionExport implements FromQuery, WithHeadings
+class TransactionExport implements FromQuery, WithHeadings, WithMapping
 {
     public int $cid;
 
@@ -19,31 +21,56 @@ class TransactionExport implements FromQuery, WithHeadings
 
     public function query()
     {
-        return Transaction::query()->where('company_id', $this->cid);
+        return Transaction::query()->where('company_id', $this->cid)->latest();
     }
 
     public function headings(): array
     {
         return [
             'ID',
-            'date',
-            'contact_id',
-            'Description',
-            'credit',
-            'debit',
-            'exchange',
-            'bolivares',
-            'currency',
-            'contact',
-            'invoice',
-            'destinatary',
-            'type',
-            'source',
-            'type_of_client',
-            'user_id',
-            'company_id',
-            'created_at',
-            'updated_at',
+            'Fecha',
+            'Divisa',
+            'Tipo',
+            'Credito',
+            'Debito',
+            'Factura',
+            'Cash/Banco',
+            'Tipo de cliente',
+            'Cliente',
+            'ID/RIF/Cedula',
+            'Cambio',
+            'Bolivares',
+            'destinatario',
+            'Empresa',
+            'Autor de la transaccion',
+            'Fecha de creacion',
+            'Ultima modification',
+            'Descripcion',
+        ];
+    }
+
+    public function map($transaction): array
+    {
+        return [
+            $transaction->id,
+            $transaction->date->format('Y-m-d'),
+            $transaction->currency,
+            $transaction->type,
+            $transaction->credit,
+            $transaction->debit,
+            $transaction->invoice, 
+            $transaction->source,           
+            $transaction->type_of_client,
+            $transaction->contact,
+            $transaction->contact_id,            
+            $transaction->exchange,
+            $transaction->bolivares,            
+            $transaction->destinatary,            
+            $transaction->company->name,            
+            $transaction->author->name,  
+            $transaction->created_at->format('Y-m-d'),          
+            $transaction->updated_at->format('Y-m-d'),  
+            $transaction->description,
         ];
     }
 }
